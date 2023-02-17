5e0a72fe2faab4cb0d276ebbca31a005781f2348
iOstik <ilyaosti@ya.ru>
2023-02-17 19:55:39 +0300

    +

---
+++ example_pics.bat
@@ -0,0 +1,17 @@
+@Echo off
+echo folder or file:
+set /p _folder=
+
+::Путь к скрипту
+set pyscript=%~dp0\prettyconverter.py
+::Удалить оригиналы файлов
+set delorig=True
+
+::Форматы файлов, которые нужно конвертировать
+set _formats=webp, jpeg, png, jpg
+::В формат
+set _output=jpg
+::Параметры ffmpeg для видео и nconvert для картинок
+set _params=-quiet -32bits -merge_alpha -ratio -rtype lanczos -rflag decr -resize 3000 0  -ratio -rtype lanczos -rflag decr -resize 0 3000 -q 83
+python "%pyscript%" %1 %_params% %_output% %_folder% %_formats%
+pause
---
+++ example_vids.bat
@@ -0,0 +1,17 @@
+@Echo off
+echo folder or file:
+set /p _folder=
+
+::Путь к скрипту
+set pyscript=%~dp0\prettyconverter.py
+::Удалить оригиналы файлов
+set delorig=True
+
+::Форматы файлов, которые нужно конвертировать
+set _formats=mp4, wmv, avi, mkv, m4v, webm, gif, ts, vob
+::В формат
+set _output=mp4
+::Параметры ffmpeg для видео и nconvert для картинок
+set _params=-c:a copy -threads 2 -preset faster -crf 21 -b:v 3000K -pix_fmt yuv420p -vf "scale=min(1280\,iw-mod(iw\,2)):min(720\,ih-mod(ih\,2)):force_original_aspect_ratio=decrease, crop=iw-mod(iw\,2):ih-mod(ih\,2)"
+python "%pyscript%" %1 %_params% %_output% %_folder% %_formats%
+pause
